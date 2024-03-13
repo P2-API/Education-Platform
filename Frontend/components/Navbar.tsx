3e258e46b722e2509cd2854f4d4465eaeb78fe7a
Lynneruup <andreaslynnerup@outlook.dk>
2024-03-13 18:31:21 +0100

    Fixed scaling issues when resizing

--- Frontend/components/Navbar.tsx
+++ Frontend/components/Navbar.tsx
@@ -10,24 +10,24 @@ const Navbar = () => {
     const path = useLocation().pathname;
     return (
         <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", height: "5em"}} >
-            <div style={{width: "20%"}}>
-                <img width={"100%"} height={"auto"} src={Logo} className="logo" alt="Logo" /> 
+            <div style={{width: "20%", minWidth: "250px", alignItems: "center"}} >
+                <img style={{width: "100%", minWidth: "250px"}} height={"auto"} src={Logo} className="logo" alt="Logo" /> 
             </div>
-            <ul style={{display: "flex", justifyContent: "space-between", width: "25%"}}>
+            <ul style={{display: "flex", justifyContent: "space-between", width: "25%", minWidth: "400px"}}>
 
-                <li style={{color: location.pathname === '/about' ? "#006eff" : undefined}}>
+                <li style={{marginLeft: "1em", marginRight: "1em", color: location.pathname === '/about' ? "#006eff" : undefined}}>
                     <Link to="/about">Om siden</Link>
                 </li>
-                <li style={{color: location.pathname === '/' ? "#006eff" : undefined}}>
+                <li style={{marginLeft: "1em", marginRight: "1em", color: location.pathname === '/' ? "#006eff" : undefined}}>
                     <Link to="/">Uddannelser</Link>
                 </li>
-                <li style={{color: location.pathname === '/methods' ? "#006eff" : undefined}}>
+                <li style={{marginLeft: "1em", marginRight: "1em", color: location.pathname === '/methods' ? "#006eff" : undefined}}>
                     <Link to="/methods">Metoder</Link>
                 </li>
             </ul>
-            <div style={{width: "20%", textAlign: "right"}}>
-                <button>Læs mere</button>
-                            </div>
+            <div style={{width: "20%", minWidth: "160px", textAlign: "right"}}>
+                <button style={{minWidth: "118px"}}>Læs mere</button>
+            </div>
         </div>
     )
 }
