ea366666c034bef6cbc9ade75b5a277b7911bf16
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2024-03-17 16:42:51 +0100

    small changes to data table

--- Frontend/pages/Homepage.tsx
+++ Frontend/pages/Homepage.tsx
@@ -5,218 +5,142 @@ import DataTable, { TableColumn } from "react-data-table-component";
 import { VscArrowCircleRight } from "react-icons/vsc";
 
 type DataRow = {
-  id: number;
-  name: string;
   løn: number;
   uddannelse: string;
   varighed: number;
-  arbejdstid: string;
-  alder: number;
-  erfaring: number;
   sprog: string;
+  branche: string,
 };
 
 const Homepage = () => {
   const tableRef = useRef<HTMLDivElement>(null);
   const columns: TableColumn<DataRow>[] = [
-    { name: "ID", selector: (row) => row.id, sortable: true },
-    { name: "Navn", selector: (row) => row.name, sortable: true },
-    { name: "Uddannelse", selector: (row) => row.uddannelse, sortable: true },
+    { name: "Uddannelse", selector: (row) => row.uddannelse, sortable: true, minWidth: "15em" },
     { name: "Varighed", selector: (row) => row.varighed, sortable: true },
-    { name: "Arbejdstid", selector: (row) => row.arbejdstid, sortable: true },
     { name: "Løn", selector: (row) => row.løn, sortable: true },
-    { name: "Alder", selector: (row) => row.alder, sortable: true },
-    { name: "Erfaring", selector: (row) => row.erfaring, sortable: true },
     { name: "Sprog", selector: (row) => row.sprog, sortable: true },
+    { name: "Branche", selector: (row) => row.branche, sortable: true }
   ];
   const data: DataRow[] = [
     {
-      id: 2,
-      name: "John Doe",
       løn: 60000,
       uddannelse: "Softwareudvikler",
       varighed: 3,
-      arbejdstid: "fuldtid",
-      alder: 28,
-      erfaring: 5,
       sprog: "Python",
+      branche: "IT"
     },
     {
-      id: 3,
-      name: "Alice Jensen",
       løn: 55000,
       uddannelse: "Computer Science",
       varighed: 4,
-      arbejdstid: "deltid",
-      alder: 30,
-      erfaring: 7,
       sprog: "JavaScript",
+      branche: "IT"
     },
     {
-      id: 4,
-      name: "Emma Nielsen",
       løn: 62000,
       uddannelse: "Datamatiker",
       varighed: 2,
-      arbejdstid: "fuldtid",
-      alder: 26,
-      erfaring: 3,
       sprog: "C#",
+      branche: "IT"
     },
     {
-      id: 5,
-      name: "Mikkel Pedersen",
       løn: 58000,
       uddannelse: "Software Engineering",
       varighed: 3,
-      arbejdstid: "fuldtid",
-      alder: 27,
-      erfaring: 4,
       sprog: "Java",
+      branche: "IT"
     },
     {
-      id: 6,
-      name: "Sarah Mikkelsen",
       løn: 57000,
       uddannelse: "Information Technology",
       varighed: 5,
-      arbejdstid: "fuldtid",
-      alder: 29,
-      erfaring: 6,
       sprog: "C++",
+      branche: "IT"
     },
     {
-      id: 7,
-      name: "Andreas Hansen",
       løn: 59000,
       uddannelse: "Computer Engineering",
       varighed: 4,
-      arbejdstid: "deltid",
-      alder: 31,
-      erfaring: 8,
       sprog: "Python",
+      branche: "IT"
     },
     {
-      id: 8,
-      name: "Louise Christensen",
       løn: 54000,
       uddannelse: "Datamatiker",
       varighed: 3,
-      arbejdstid: "fuldtid",
-      alder: 28,
-      erfaring: 5,
       sprog: "JavaScript",
+      branche: "IT"
     },
     {
-      id: 9,
-      name: "Peter Larsen",
       løn: 61000,
       uddannelse: "Softwareudvikler",
       varighed: 4,
-      arbejdstid: "fuldtid",
-      alder: 30,
-      erfaring: 7,
       sprog: "C#",
+      branche: "IT"
     },
     {
-      id: 10,
-      name: "Maria Olsen",
       løn: 60000,
       uddannelse: "Computer Science",
       varighed: 2,
-      arbejdstid: "fuldtid",
-      alder: 25,
-      erfaring: 3,
       sprog: "Java",
+      branche: "IT"
     },
     {
-      id: 11,
-      name: "Nikolaj Rasmussen",
       løn: 56000,
       uddannelse: "Software Engineering",
       varighed: 3,
-      arbejdstid: "fuldtid",
-      alder: 27,
-      erfaring: 4,
       sprog: "C++",
+      branche: "IT"
     },
     {
-      id: 12,
-      name: "Sofie Jørgensen",
       løn: 58000,
       uddannelse: "Information Technology",
       varighed: 5,
-      arbejdstid: "fuldtid",
-      alder: 29,
-      erfaring: 6,
       sprog: "Python",
+      branche: "IT"
     },
     {
-      id: 13,
-      name: "Martin Madsen",
       løn: 57000,
       uddannelse: "Computer Engineering",
       varighed: 4,
-      arbejdstid: "deltid",
-      alder: 31,
-      erfaring: 8,
       sprog: "JavaScript",
+      branche: "IT"
     },
     {
-      id: 14,
-      name: "Line Andersen",
       løn: 59000,
       uddannelse: "Datamatiker",
       varighed: 3,
-      arbejdstid: "fuldtid",
-      alder: 28,
-      erfaring: 5,
       sprog: "C#",
+      branche: "IT"
     },
     {
-      id: 15,
-      name: "Anders Christensen",
       løn: 61000,
       uddannelse: "Softwareudvikler",
       varighed: 4,
-      arbejdstid: "fuldtid",
-      alder: 30,
-      erfaring: 7,
       sprog: "Java",
+      branche: "IT"
     },
     {
-      id: 16,
-      name: "Camilla Nielsen",
       løn: 60000,
       uddannelse: "Computer Science",
       varighed: 2,
-      arbejdstid: "fuldtid",
-      alder: 25,
-      erfaring: 3,
       sprog: "C++",
+      branche: "IT"
     },
     {
-      id: 17,
-      name: "Lars Pedersen",
       løn: 56000,
       uddannelse: "Information Technology",
       varighed: 5,
-      arbejdstid: "fuldtid",
-      alder: 29,
-      erfaring: 6,
       sprog: "Python",
+      branche: "IT"
     },
     {
-      id: 18,
-      name: "Louise Hansen",
       løn: 59000,
       uddannelse: "Computer Engineering",
       varighed: 4,
-      arbejdstid: "deltid",
-      alder: 31,
-      erfaring: 8,
       sprog: "JavaScript",
-    },
+      branche: "IT"
+    }
   ];
 
   return (
@@ -267,7 +191,7 @@ const Homepage = () => {
           style={{ minWidth: "462px", height: "100%" }}
         />
       </div>
-      <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <div
           className="crazy-shadow"
           style={{
@@ -280,14 +204,20 @@ const Homepage = () => {
             columns={columns}
             highlightOnHover
             pointerOnHover
+            fixedHeader
+            fixedHeaderScrollHeight="25em"
             data={data}
             responsive
             expandableRows
             expandOnRowClicked
-            style={{
-              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-              overflow: "scroll",
-            }}
+            customStyles={{
+              table: {
+                style: {
+                  width: "100%",
+                },
+              },
+            }
+            }
           />
         </div>
       </div>
