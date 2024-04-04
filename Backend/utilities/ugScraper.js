e741afe7d9c47e95a8054d1e9512a24cf09af2c9
Lynneruup <76641014+Lynneruup@users.noreply.github.com>
2024-04-04 11:41:40 +0200

    update

--- Backend/utilities/ugScraper.js
+++ Backend/utilities/ugScraper.js
@@ -93,7 +93,7 @@ async function calculateSimilarities(topics, wordList) {
 
 (async () => {
 
-    const url = 'https://www.ug.dk/uddannelser/bachelorogkandidatuddannelser/bacheloruddannelser/naturvidenskabeligebacheloruddannelser/matematikfysikkemiogdatalogi/datalogi';
+    const url = 'https://www.ug.dk/uddannelser/bachelorogkandidatuddannelser/bacheloruddannelser/sundhedsvidenskabeligebacheloruddannelser/medicin';
     const htmlData = await fetchHtmlData(url);
     const text = await filterData(htmlData) + '. ' + await filterData2(htmlData);
     const useableText = addSpaceBeforeUppercase(text);
