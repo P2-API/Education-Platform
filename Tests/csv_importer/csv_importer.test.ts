9bf1318cb6c9605ef400933633dd26c675f18d96
PayThePro <patrick@reiffenste.in>
2024-05-02 17:07:33 +0200

    Made the test for the csv import, don't know if it works since I don't know how to run the tests yet.

--- Tests/csv_importer/csv_importer.test.ts
+++ Tests/csv_importer/csv_importer.test.ts
@@ -1,4 +1,19 @@
 // Make a csv turn into json. 
 
-import { test } from "vitest";
+import { readFileSync } from "fs";
+import { test, expect } from "vitest";
 
+
+// Input is CSV_import_test.csv and output is JSON_output_test.json
+// The correct result is the file JSON_correct.json
+test('csv import as json', () => {
+    function import_csv() {
+        
+    }
+    
+
+    var output = readFileSync('Tests\csv_importer\JSON_output_test.json');
+    var answer = readFileSync('Tests\csv_importer\JSON_correct.json'); // TODO: This file is currently empty
+
+    expect(output).toBe(answer);
+})
