04eed10bca9cff9b96b79843e5e1936d21caa2b1
PayThePro <patrick@reiffenste.in>
2024-05-07 11:13:05 +0200

    Tweaked test for csv importer so it works and made the csv_importer so I works a bit. It still needs a couple of features. Tried fixing coverage folder by adding to gitignore

--- Tests/csv_importer/csv_importer.test.ts
+++ Tests/csv_importer/csv_importer.test.ts
@@ -1,16 +1,14 @@
-// Make a csv turn into json. 
+// Make a csv turn into json.
 import { test, expect } from "vitest";
 import { import_csv } from "../../Backend/utilities/csv_importer"
+import * as testFile from "./output_test"
+import * as correctAnswer from "./output_correct"
 
-// Input is CSV_import_test.csv and output is output_test.ts
-// The correct result is the file output_correct.ts
-import_csv('Tests/csv_import_test.csv', 'Tests/csv_importer/output_test.ts')
-
-//Get the files
-import { educations_correct } from "./output_correct"
-import { educations } from "./output_test"
-
-// Run the test
+// Run the test 
 test('csv imported correctly', () => {
-    expect(JSON.stringify(educations)).toBe(JSON.stringify(educations_correct));
+    // Input is CSV_import_test.csv and output is output_test.ts
+    // The correct result is the file output_correct.ts
+    import_csv('Tests/csv_importer/csv_import_test.csv', 'Tests/csv_importer/output_test.ts');
+
+    expect(testFile).toStrictEqual(correctAnswer);
 })
