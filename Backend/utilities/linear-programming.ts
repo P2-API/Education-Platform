926fc01b607fd715a5d8617fd21e45e0947d2641
Lukas Saltenis <tl00xu@student.aau.dk>
2024-05-08 14:00:53 +0200

    hej

--- Backend/utilities/linear-programming.ts
+++ Backend/utilities/linear-programming.ts
@@ -1,25 +1,63 @@
 import {GLPK, LP, Result} from 'glpk.js/dist/glpk';
-import {UserImputs, objectiveType, SubjectToType, TableFilters, QuizAnswers} from '../../src/types'
+import * as Types from '../../src/types';
 import GLPKConstructor from 'glpk.js/dist/glpk';
 
 export class LPclass implements LP{
   name: string;
-  objective: objectiveType;
-  subjectTo: SubjectToType;
-  constructor(){
+  objective: Types.objectiveType;
+  subjectTo: Types.SubjectToType;
+  constructor(solver:GLPK){
     this.name = "Optimal education";
-    this.objective = {direction: 1, name: "", vars: []};
+    this.objective = {direction: solver.GLP_MAX, name: "Optimal Education", vars: []};
     this.subjectTo = [];
   }
-  addObjectiveFunction(quizAnswers:QuizAnswers):void{
-    let key:string;
-    let value:number;
-    for([key, value] of Object.entries(quizAnswers)){
-      this.objective.vars.push({name:key, coef:value})
-    }
-   
+  addObjectiveFunction(quizAnswers:Types.QuizAnswers, filters:Types.TableFilters):void{
+    const variables = this.objective.vars
+    // add weights to:
+    // subjects
+    filters.hasSubjects.forEach((subject) =>{
+      variables.push({name: subject, coef:quizAnswers.subjectsPriority})
+    })
+    // industries
+    filters.hasIndustries.forEach((industry) =>{
+      variables.push({name: industry, coef:quizAnswers.subjectsPriority})
+    })
+    // hours
+    variables.push({name: "withManyStudents", coef:quizAnswers.highWorkloadAcceptancePriority},
+                  {name: "withFewStudents", coef:quizAnswers.highWorkloadAcceptancePriority},
+                  {name: "withManyLectures", coef:quizAnswers.highWorkloadAcceptancePriority},)
+  
+    // socialFeedback
+    variables.push({name: "socialEnvironment", coef:quizAnswers.socialEnvironmentPriority},
+                  {name: "groupEngagement", coef:quizAnswers.groupEngagementPriority},
+                  {name: "loneliness", coef:quizAnswers.lonelinessPriority},
+                  {name: "stress", coef:quizAnswers.stressPriority})
+    // academicFeedback
+    variables.push({name: "academicEnvironment", coef:quizAnswers.academicEnvironmentPriority},
+                  {name: "teacherEvalutation", coef:quizAnswers.teachingPriority})
+    // academicWorkload
+    variables.push({name: "studentJob", coef:quizAnswers.studentJobPriority},
+                  {name: "lectures", coef:quizAnswers.lecturesPriority},
+                  {name: "literature", coef:quizAnswers.literaturePriority})
+    // degreeStructure
+    variables.push({name: "dislikeExam", coef:quizAnswers.dislikeExamPriority},
+                  {name: "internship", coef:quizAnswers.internshipPriority},
+                  {name: "internationalStay", coef:quizAnswers.internationalStayPriority})
+    // jobData
+    variables.push({name: "workNationally", coef:quizAnswers.workNationallyPriority},
+                  {name: "startingSalary", coef:quizAnswers.startingSalaryPriority},
+                  {name: "experiencedSalary", coef:quizAnswers.experiencedSalaryPriority},
+                  {name: "unemployment", coef:quizAnswers.unemploymentPriority},
+                  {name: "degreeRelevance", coef:quizAnswers.degreeRelevancePriority},
+                  {name: "fixedHours", coef:quizAnswers.fixedHoursPriority},
+                  {name: "flexibleHours", coef:quizAnswers.flexibleHoursPriority},
+                  {name: "selfSchedule", coef:quizAnswers.selfSchedulePriority},
+                  {name: "variableSchedule", coef:quizAnswers.variableSchedulePriority},
+                  {name: "nightAndEveningShifts", coef:quizAnswers.nightAndEveningShiftsPriority},
+                  {name: "workNationally", coef:quizAnswers.workNationallyPriority})
+  
   }
-  addConstraints(filters:TableFilters):void{
+  addConstraints(filters:Types.TableFilters):void{
     
   }
 }
@@ -29,19 +67,19 @@ export class LPsolver{
   constructor(){
     this.solver = GLPKConstructor();
   }
-  solve(userImputs:UserImputs):Result{
+  solve(userImputs:Types.UserImputs):Result{
     const lp:LP = this.setUpLP(userImputs);
     return this.solver.solve(lp);
   }
-  setUpLP(userImputs:UserImputs):LP{
-    const lp = new LPclass()
-    lp.addObjectiveFunction(userImputs.quizAnswers);
+  setUpLP(userImputs:Types.UserImputs):LP{
+    const lp = new LPclass(this.solver)
+    lp.addObjectiveFunction(userImputs.quizAnswers, userImputs.filters);
     lp.addConstraints(userImputs.filters);
     return lp;
   }
 }
 
-export function findOptimalSolution(userImputs:UserImputs):Result{
+export function findOptimalSolution(userImputs:Types.UserImputs):Result{
   const solver = new LPsolver();
   return solver.solve(userImputs);
 }
