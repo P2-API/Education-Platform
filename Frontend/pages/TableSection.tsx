5516400951315ea08478b9df8df2dc546c9faeae
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-05-08 11:54:47 +0200

    Moved calculations to server
    Moved all of table sections calculations to server start up

--- Frontend/pages/TableSection.tsx
+++ Frontend/pages/TableSection.tsx
@@ -1,7 +1,7 @@
 import MaterialReactDataTable from "../components/DataTable";
 import { DegreeType, DegreeTypeToDuration, Geography, Institution } from "../../src/enums";
 import { MultiSelectAutoComplete, MinimumDistanceSlider } from "../components/Filters";
-import { Education, MinimumMaximum } from "types";
+import { Education, MinimumMaximum, TableSectionDataFromServer } from "types";
 
 import React from 'react';
 import Paper from '@mui/material/Paper';
@@ -12,71 +12,14 @@ import Paper from '@mui/material/Paper';
 type TableSectionProps = {
     tableRef: React.RefObject<HTMLDivElement>;
     setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    possibleEducations: Education[];
+    data: TableSectionDataFromServer | undefined;
 };
 
 
 
-const TableSection: React.FC<TableSectionProps> = ({ tableRef, setIsModalOpen, possibleEducations }) => {
-    // Enum Lists
-    const degreeTypeKeys: (keyof typeof DegreeType)[] = Object.keys(DegreeType)
-        .filter(key => isNaN(Number(key))) as (keyof typeof DegreeType)[];
-    const degreeTypes: DegreeType[] = degreeTypeKeys.map(key => DegreeType[key]);
-
-    const institutionKeys: (keyof typeof Institution)[] = Object.keys(Institution)
-        .filter(key => isNaN(Number(key))) as (keyof typeof Institution)[];
-    const institutes: Institution[] = institutionKeys.map(key => Institution[key]);
-
-    const geographyKeys: (keyof typeof Geography)[] = Object.keys(Geography)
-        .filter(key => isNaN(Number(key))) as (keyof typeof Geography)[];
-    const geographies: Geography[] = geographyKeys.map(key => Geography[key]);
-
-    const degreeTypesString = degreeTypeKeys.map(value => value.toString());
-    const institutesString = institutionKeys.map(value => value.toString());
-    const geographiesString = geographyKeys.map(value => value.toString());
-
-    // Education Duration Slider
-    let educationDurationMin = DegreeTypeToDuration(degreeTypes[0]).minimum;
-    let educationDurationMax = DegreeTypeToDuration(degreeTypes[0]).maximum;
-    degreeTypes.forEach((degreeType) => {
-        let newDuration = DegreeTypeToDuration(degreeType);
-        if (newDuration.minimum != -1) {
-            educationDurationMin = Math.min(educationDurationMin, newDuration.minimum);
-            educationDurationMax = Math.max(educationDurationMax, newDuration.maximum);
-        }
-    });
-
-    const educationDurationRange: MinimumMaximum = { minimum: educationDurationMin, maximum: educationDurationMax };
+const TableSection: React.FC<TableSectionProps> = ({ tableRef, setIsModalOpen, data }) => {
     const getValueTextDuration = (value: number) => { return `${value} måneder`; }
-
-    // Possible Educations
-    let newGraduateSalaryMin: number;
-    let experiencedSalaryMin: number;
-    let newGraduateSalaryMax: number;
-    let experiencedSalaryMax: number;
-
-    let newGraduateSalaryRange: MinimumMaximum = { minimum: -1, maximum: -1 };
-    let experiencedSalaryRange: MinimumMaximum = { minimum: -1, maximum: -1 };
-
-    if (possibleEducations.length > 0) {
-        newGraduateSalaryMin = possibleEducations[0].job_data.salaries.newGraduate.lower_quartile;
-        experiencedSalaryMin = possibleEducations[0].job_data.salaries.experienced.lower_quartile;
-        newGraduateSalaryMax = possibleEducations[0].job_data.salaries.newGraduate.upper_quartile;
-        experiencedSalaryMax = possibleEducations[0].job_data.salaries.experienced.upper_quartile;
-        possibleEducations.forEach((education) => {
-            newGraduateSalaryMin = Math.min(newGraduateSalaryMin, education.job_data.salaries.newGraduate.lower_quartile);
-            experiencedSalaryMin = Math.min(experiencedSalaryMin, education.job_data.salaries.experienced.lower_quartile);
-            newGraduateSalaryMax = Math.max(newGraduateSalaryMax, education.job_data.salaries.newGraduate.upper_quartile);
-            experiencedSalaryMax = Math.max(experiencedSalaryMax, education.job_data.salaries.experienced.upper_quartile);
-        });
-        newGraduateSalaryRange = { minimum: newGraduateSalaryMin, maximum: newGraduateSalaryMax };
-        experiencedSalaryRange = { minimum: experiencedSalaryMin, maximum: experiencedSalaryMax };
-    }
-
     const getValueTextSalary = (value: number) => { return `${value}k kr.` }
-
-    // Misc
-
 
     return (
         <div style={{ height: "100vh", width: "100%", backgroundColor: "#f8fbff" }}>
@@ -92,34 +35,36 @@ const TableSection: React.FC<TableSectionProps> = ({ tableRef, setIsModalOpen, p
                             <button className="primary-button" style={{ marginRight: "0.5em", borderRadius: 5 }} onClick={() => setIsModalOpen(true)}>Quiz</button>
                         </div>
                         <div style={{ padding: "1em", display: "grid", gap: "1em", height: "100%" }}>
-                            <MultiSelectAutoComplete collection={degreeTypesString} selectLabel="Filtrer efter uddannelsestype" selectPlaceholder="Uddannelsestype" />
-                            <MultiSelectAutoComplete collection={institutesString} selectLabel="Filtrer efter uddannelsessted" selectPlaceholder="Uddannelsessted" />
-                            <MultiSelectAutoComplete collection={geographiesString} selectLabel="Filtrer efter kommune" selectPlaceholder="Kommune" />
-                            <MultiSelectAutoComplete collection={geographiesString} selectLabel="Filtrer efter kommune" selectPlaceholder="Kommune" />
-                            <MinimumDistanceSlider
-                                initialState={educationDurationRange}
-                                sliderRange={educationDurationRange}
-                                minimumDistance={1}
-                                description="Filtrer efter uddannelsesvarighed i måneder"
-                                getValueText={getValueTextDuration}
-                            />
-                            {(possibleEducations.length > 0) && (
-                                <MinimumDistanceSlider
-                                    initialState={newGraduateSalaryRange}
-                                    sliderRange={newGraduateSalaryRange}
-                                    minimumDistance={1}
-                                    description="Filtrer efter nyuddannedes løn i tusinde"
-                                    getValueText={getValueTextSalary}
-                                />
-                            )}
-                            {(possibleEducations.length > 0) && (
-                                <MinimumDistanceSlider
-                                    initialState={experiencedSalaryRange}
-                                    sliderRange={experiencedSalaryRange}
-                                    minimumDistance={1}
-                                    description="Filtrer efter erfarenes løn i tusinde"
-                                    getValueText={getValueTextSalary}
-                                />
+                            {(data != undefined) && (
+                                <>
+                                    <MultiSelectAutoComplete collection={data.degreeTypesString} selectLabel="Filtrer efter uddannelsestype" selectPlaceholder="Uddannelsestype" />
+                                    <MultiSelectAutoComplete collection={data.institutesString} selectLabel="Filtrer efter uddannelsessted" selectPlaceholder="Uddannelsessted" />
+                                    <MultiSelectAutoComplete collection={data.geographiesString} selectLabel="Filtrer efter kommune" selectPlaceholder="Kommune" />
+                                    <MultiSelectAutoComplete collection={data.geographiesString} selectLabel="Filtrer efter kommune" selectPlaceholder="Kommune" />
+                                    <MinimumDistanceSlider
+                                        initialState={data.educationDurationRange}
+                                        sliderRange={data.educationDurationRange}
+                                        minimumDistance={1}
+                                        description="Filtrer efter uddannelsesvarighed i måneder"
+                                        getValueText={getValueTextDuration}
+                                    />
+                                    <MinimumDistanceSlider
+                                        initialState={data.newGraduateSalaryRange}
+                                        sliderRange={data.newGraduateSalaryRange}
+                                        minimumDistance={1}
+                                        description="Filtrer efter nyuddannedes løn i tusinde"
+                                        getValueText={getValueTextSalary}
+                                    />
+                                
+                                    <MinimumDistanceSlider
+                                        initialState={data.experiencedSalaryRange}
+                                        sliderRange={data.experiencedSalaryRange}
+                                        minimumDistance={1}
+                                        description="Filtrer efter erfarenes løn i tusinde"
+                                        getValueText={getValueTextSalary}
+                                    />
+                                </>
+                                
                             )}
 
                             <p>Filter efter uddannelsesstart</p>
