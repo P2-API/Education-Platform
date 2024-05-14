57a18f9e70e054b250e67a084065e4a09a2c8ff5
Lukas Saltenis <tl00xu@student.aau.dk>
2024-05-14 12:50:45 +0200

    types modified

--- src/types.ts
+++ src/types.ts
@@ -115,11 +115,6 @@ export type Education = {
                         self_schedule_priority: number; variable_schedule_priority: number; night_and_evening_shifts_priority: number; work_nationally_priority: number;*/
 };
 
-export type RankingResult = [{
-    EducationId: number;
-    ranking: number;
-}]
-
 export type QuizAnswers = {
 
     subjectsPriority: number; // in use
