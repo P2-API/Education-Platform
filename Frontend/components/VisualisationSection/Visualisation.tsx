a374f36e57f366df7bed21af64c1b345ab028cc8
PayThePro <patrick@reiffenste.in>
2024-05-14 13:40:18 +0200

    Added support for changing between graphs in visualization

--- Frontend/components/VisualisationSection/Visualisation.tsx
+++ Frontend/components/VisualisationSection/Visualisation.tsx
@@ -2,20 +2,23 @@ import React, { useEffect } from 'react';
 import { Paper } from '@mui/material';
 import Plot from 'react-plotly.js';
 
-import { chartType } from '@frontend/pages/VisualisationSection';
+import { ChartType } from "./VisualisationSettingsBox"
 
 interface VisualisationProps {
-    chartType: chartType
+    chartType: ChartType
 }
 
-const Visualisation: React.FC<VisualisationProps> = () => {
 
+const Visualisation: React.FC<VisualisationProps> = ({ chartType }) => {
+
+    console.log("chartType: ", chartType)
+    
     const [data, setData] = React.useState<{ x: number[], y: number[], text: string[] }>({
         x: [1, 2, 3, 4, 5],
         y: [1, 2, 3, 4, 5],
         text: ["Point 1", "Point 2", "Point 3", "Point 4", "Point 5"]
-    });
-
+    }); 
+    
     const generateRandomData = () => {
         const xValues = [];
         const yValues = [];
@@ -29,11 +32,102 @@ const Visualisation: React.FC<VisualisationProps> = () => {
         setData({ x: xValues, y: yValues, text: text });
     };
 
-    useEffect(() => {
-        generateRandomData();
-    }, []);
+    const scatterPlot = (
+        <Paper elevation={2} style={{ height: "100%", zIndex: 1, width: "100%", overflowY: "scroll" }}>
+        <button onClick={generateRandomData} >Randomize</button>
+        <div style={{ display: "grid", gap: "1em", height: "95%", }}>
+            <Plot
+                data={[
+                    {
+                        x: data.x,
+                        y: data.y,
+                        text: data.text,
+                        mode: "markers",
+                        type: "scatter",
+                        marker: { size: 10, sizemode: "area", colorscale: "Viridis" },
+                        hovertemplate:
+                            "<b>%{text}</b><br><br>" +
+                            "Løn: %{y}<br>" +
+                            "Uddannelsestype: %{x}<br>" +
+                            "<extra></extra>"
+                    }
+                ]}
+                layout={{
+                    title: "PCA Analyse - Løn og uddannelsestype",
+                    xaxis: {
+                        title: "Uddannelsestype",
+                        range: [0, 10] // Set the range for x-axis
+                    },
+                    yaxis: {
+                        title: "Løn (kr.)",
+                        range: [0, 100] // Set the range for y-axis
+                    },
+                    hovermode: "closest",
+                    hoverlabel: { bgcolor: "#FFF" },
+                    autosize: true, // Enable automatic resizing
+                    transition: {
+                        duration: 500,
+                        easing: 'cubic-in-out'
+                    },
+                }}
+                config={{
+                    responsive: true,
+                }}
 
-    return (
+            />
+        </div>
+    </Paper >
+    );
+
+
+    const barPlot = (
+    <Paper elevation={2} style={{ height: "100%", zIndex: 1, width: "100%", overflowY: "scroll" }}>
+        <button onClick={generateRandomData} >Randomize</button>
+        <div style={{ display: "grid", gap: "1em", height: "95%", }}>
+            <Plot
+                data={[
+                    {
+                        x: data.x,
+                        y: data.y,
+                        text: data.text,
+                        mode: "markers",
+                        type: "scatter",
+                        marker: { size: 10, sizemode: "area", colorscale: "Viridis" },
+                        hovertemplate:
+                            "<b>%{text}</b><br><br>" +
+                            "Løn: %{y}<br>" +
+                            "Uddannelsestype: %{x}<br>" +
+                            "<extra></extra>"
+                    }
+                ]}
+                layout={{
+                    title: "PCA Analyse - Løn og uddannelsestype",
+                    xaxis: {
+                        title: "Uddannelsestype",
+                        range: [0, 10] // Set the range for x-axis
+                    },
+                    yaxis: {
+                        title: "Løn (kr.)",
+                        range: [0, 100] // Set the range for y-axis
+                    },
+                    hovermode: "closest",
+                    hoverlabel: { bgcolor: "#FFF" },
+                    autosize: true, // Enable automatic resizing
+                    transition: {
+                        duration: 500,
+                        easing: 'cubic-in-out'
+                    },
+                }}
+                config={{
+                    responsive: true,
+                }}
+
+            />
+        </div>
+    </Paper >
+    );
+
+    const radarPlot = (
         <Paper elevation={2} style={{ height: "100%", zIndex: 1, width: "100%", overflowY: "scroll" }}>
             <button onClick={generateRandomData} >Randomize</button>
             <div style={{ display: "grid", gap: "1em", height: "95%", }}>
@@ -74,11 +168,25 @@ const Visualisation: React.FC<VisualisationProps> = () => {
                     config={{
                         responsive: true,
                     }}
-
+    
                 />
             </div>
         </Paper >
-    );
-};
+        );
+
+    useEffect(() => {
+        generateRandomData();
+    }, []);
+
+
+    return (
+        ((chartType == ChartType.scatter) && scatterPlot)
+        ||
+        ((chartType == ChartType.bar) && barPlot)
+        ||
+        ((chartType == ChartType.radar) && radarPlot)
+    )
+}
+
 
 export default Visualisation;
