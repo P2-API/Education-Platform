f64175172ecdcc2dc43649dc7736cc6394efc285
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-05-16 09:26:13 +0200

    Commented tests
    Commented tests:
    - normalization.test.ts
    - calculate-minimum-and-maximum-education.test.ts
    - calculate-minimum-and-maximum-degree-duration.test.ts
    - deep-copy.test.ts

--- Tests/csv_importer/uddannelseszoom_helper_functions/normalization.test.ts
+++ Tests/csv_importer/uddannelseszoom_helper_functions/normalization.test.ts
@@ -4,19 +4,19 @@ import normilize from '../../../Backend/utilities/normalization'
 
 
 test("Normalization", () => {
-    let number = 30;
-    let min = 0;
-    let max = 100;
+    let number = 30; // Mock number
+    let min = 0; // Mock minimum
+    let max = 100; // Mock maximum
     expect(normilize(number, min, max)).toStrictEqual(0.3);
 
-    number = -2;
+    number = -2; // Mock number
     expect(normilize(number, min, max)).toStrictEqual(-0.02);
 
-    number = 200;
+    number = 200; // Mock number
     expect(normilize(number, min, max)).toStrictEqual(2);
 
-    number = 4.43;
-    min = 70.8;
-    max = 100.3;
-    expect(normilize(number, min, max)).closeTo(-2.24983050847458, 0.00000000001);
+    number = 4.43; // Mock number
+    min = 70.8; // Mock minimum
+    max = 100.3; // Mock maximum
+    expect(normilize(number, min, max)).closeTo(-2.24983050847458, 0.00000000001); // Close to the expected value, due to floating point precision
 });
