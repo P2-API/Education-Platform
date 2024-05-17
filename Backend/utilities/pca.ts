72954073bad1a2b8e87b4b3c611f60438ee0786e
Lukas Saltenis <tl00xu@student.aau.dk>
2024-05-17 15:47:15 +0200

    pca data should be correct now

--- Backend/utilities/pca.ts
+++ Backend/utilities/pca.ts
@@ -1,11 +1,14 @@
-import { EducationData, EducationVector, PCAData, QuizAnswers } from "../../src/types";
+import { EducationData, EducationVector, PCAData, QuizAnswers} from "../../src/types";
 import { Ranker } from "./ranking";
 import { PCA, PCAOptions} from "ml-pca";
 import { getEducationData } from "../server/on-server-start";
 
 export function performPCA(quizAnswers:QuizAnswers):PCAData{
     const educations:EducationVector[] = constructEducationVecotors(quizAnswers);
+    //console.log("coordinates", educations.map((education) => education.coordinates))
     const dataset = constructMatrix(educations)
+    //console.log("dataset", dataset)
+
     const pcaSettings:PCAOptions = {
         scale:true,                 // standerdize the data
         ignoreZeroVariance:true,    // ignore columns with zero variance for faster computation
@@ -37,23 +40,23 @@ function pcaData(educations:EducationVector[], transformedData:number[][], Pca:P
     const eigenvectors:number[][] = Pca.getEigenvectors().to2DArray();
     
     Object.keys(pcaData.principalComponents).forEach((axis, index) => {
-        pcaData.principalComponents[axis].varienceExplained = explainedVariance[index] //explainedVariance property is filled in for each axis
+        pcaData.principalComponents[axis].varianceExplained = explainedVariance[index] //explainedVariance property is filled in for each axis
         eigenvectors[index].forEach((coefficient, i) => {
             pcaData.principalComponents[axis].composition.push({variable: educations[0].coordinates[i].name, coeff: coefficient}) // the composition property is filled in for each axis
         })
     })
-
     return pcaData
 }
 
 function constructEducationVecotors(weights:QuizAnswers):EducationVector[]{
     const educations:EducationData = getEducationData()
     const educationVectors:EducationVector[] = []
+    //console.log("educations", educations.normalized)
     const ranker = new Ranker()
     
     educations.normalized.forEach((education) => {
-        ranker.educationVector(education, weights)
+        educationVectors.push(ranker.educationVector(education, weights))
     })
-
+    
     return educationVectors
 }
