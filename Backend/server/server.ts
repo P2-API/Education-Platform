ad41dd26d05098648378363f0944792b5a20b3fb
Lukas Saltenis <tl00xu@student.aau.dk>
2024-05-21 13:19:19 +0200

    pca works

--- Backend/server/server.ts
+++ Backend/server/server.ts
@@ -31,8 +31,8 @@ server.get("/get_table_section_data", (request: Request, response: Response) => 
     response.status(200).send(getTableSectionData());
 });
 server.post("/PCA_request", (request: Request, response: Response) => {
-    const requestData = request.body;
-    const pcaData = performPCA(requestData.quizAnswers);
+    const requestData:UserInputs = request.body;
+    const pcaData = performPCA(requestData);
     response.status(200).send(pcaData);
 });
 server.get("/get_grouped_educations", (request: Request, response: Response) => {
